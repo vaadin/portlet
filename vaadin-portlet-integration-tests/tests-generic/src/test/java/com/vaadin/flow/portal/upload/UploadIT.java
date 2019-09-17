f49ef4b441bdb31d2fe5f4c17b0ad1c4b75ef52f
Johannes Eriksson <joheriks@vaadin.com>
2019-09-17 15:13:53 +0300

    Upload support and IT (portlet-api version 3.0.0 required) (#22)
    * Upload support and IT (portlet-api version 3.0.0 required)
    
    * Updated upload test to work on hub
    
    * Addressed some Sonar issues
    
    * Added license headers and JavaDocs
    
    * Use try-with-resources construct

---
+++ vaadin-portlet-integration-tests/tests-generic/src/test/java/com/vaadin/flow/portal/upload/UploadIT.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2000-2019 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.portal.upload;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WrapsElement;
+import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import com.vaadin.flow.component.html.testbench.LabelElement;
+import com.vaadin.flow.component.upload.testbench.UploadElement;
+import com.vaadin.flow.portal.AbstractPlutoPortalTest;
+
+public class UploadIT extends AbstractPlutoPortalTest {
+
+    public UploadIT() {
+        super("upload");
+    }
+
+    @Test
+    public void testUpload() throws Exception {
+        // upload a file
+        File file = createTempFile();
+        UploadElement upload = $(UploadElement.class).first();
+        WebElement input = getInShadowRoot(upload, By.id("fileInput"));
+        setLocalFileDetector(input);
+        input.sendKeys(file.getAbsolutePath());
+
+        // check that label indicates size of file
+        LabelElement label = $(LabelElement.class).id(UploadPortletContent.UPLOAD_LABEL_ID);
+        Assert.assertEquals(Long.toString(file.length()), label.getText());
+    }
+
+    private File createTempFile() throws IOException {
+        File tempFile = File.createTempFile("test", ".txt");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+        writer.write("These are the contents of the uploaded file.");
+        writer.close();
+        tempFile.deleteOnExit();
+        return tempFile;
+    }
+
+    private void setLocalFileDetector(WebElement element) throws Exception {
+        if (getRunLocallyBrowser() != null) {
+            return;
+        }
+
+        if (element instanceof WrapsElement) {
+            element = ((WrapsElement) element).getWrappedElement();
+        }
+        if (element instanceof RemoteWebElement) {
+            ((RemoteWebElement) element)
+                    .setFileDetector(new LocalFileDetector());
+        } else {
+            throw new IllegalArgumentException(
+                    "Expected argument of type RemoteWebElement, received "
+                            + element.getClass().getName());
+        }
+    }
+}
---
+++ vaadin-portlet/src/main/java/com/vaadin/flow/portal/PortletStreamReceiverHandler.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2000-2019 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.portal;
+
+import javax.portlet.ClientDataRequest;
+import javax.portlet.PortletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.RequestContext;
+import org.apache.commons.fileupload.portlet.PortletFileUpload;
+
+import com.vaadin.flow.server.VaadinRequest;
+import com.vaadin.flow.server.communication.StreamReceiverHandler;
+
+/**
+ * Extends {@link StreamReceiverHandler} to handle upload
+ * {@link VaadinRequest}s where the underlying request is of type
+ * {@link ClientDataRequest} instead of {@link HttpServletRequest}.
+ */
+public class PortletStreamReceiverHandler extends StreamReceiverHandler {
+
+    @Override
+    protected boolean isMultipartUpload(VaadinRequest request) {
+        return request instanceof VaadinPortletRequest &&
+                PortletFileUpload.isMultipartContent(getRequestContext(request));
+    }
+
+    @Override
+    protected Collection<Part> getParts(VaadinRequest request)
+            throws Exception {
+        PortletRequest portletRequest = getPortletRequest(request);
+        return ((ClientDataRequest) portletRequest).getParts();
+    }
+
+    @Override
+    protected FileItemIterator getItemIterator(VaadinRequest request)
+            throws FileUploadException, IOException {
+        PortletFileUpload upload = new PortletFileUpload();
+        return upload.getItemIterator(getRequestContext(request));
+    }
+
+    private PortletRequest getPortletRequest(VaadinRequest request) {
+        return ((VaadinPortletRequest) request).getPortletRequest();
+    }
+
+    private RequestContext getRequestContext(VaadinRequest request) {
+        return new RequestContext() {
+            @Override
+            public String getCharacterEncoding() {
+                return request.getCharacterEncoding();
+            }
+
+            @Override
+            public String getContentType() {
+                return request.getContentType();
+            }
+
+            @Override
+            public int getContentLength() {
+                return request.getContentLength();
+            }
+
+            @Override
+            public InputStream getInputStream() throws IOException {
+                return request.getInputStream();
+            }
+        };
+    }
+}

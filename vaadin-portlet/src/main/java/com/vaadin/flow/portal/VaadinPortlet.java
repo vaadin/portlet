8f029aaaad00bebbfb2a8f5d8a013d302fdae90c
Mikael Grankvist <mikael.grankvist@vaadin.com>
2019-10-15 13:40:38 +0300

    Programmatic mode and state change
    Add methods to programmatically change
    WindowState and PortletMode.
    
    Added fallback for 2.0 using action
    
    Fixes #37
    Fixes #47

--- vaadin-portlet/src/main/java/com/vaadin/flow/portal/VaadinPortlet.java
+++ vaadin-portlet/src/main/java/com/vaadin/flow/portal/VaadinPortlet.java
@@ -17,19 +17,24 @@ package com.vaadin.flow.portal;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.ActionURL;
 import javax.portlet.EventRequest;
 import javax.portlet.EventResponse;
 import javax.portlet.GenericPortlet;
+import javax.portlet.HeaderRequest;
+import javax.portlet.HeaderResponse;
 import javax.portlet.PortalContext;
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletContext;
 import javax.portlet.PortletException;
+import javax.portlet.PortletMode;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
+import javax.portlet.WindowState;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Locale;
@@ -37,10 +42,11 @@ import java.util.Properties;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.internal.ExportsWebComponent;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.internal.ExportsWebComponent;
 import com.vaadin.flow.function.DeploymentConfiguration;
 import com.vaadin.flow.internal.CurrentInstance;
+import com.vaadin.flow.portal.util.PortletHubUtil;
 import com.vaadin.flow.server.Command;
 import com.vaadin.flow.server.Constants;
 import com.vaadin.flow.server.DefaultDeploymentConfiguration;
@@ -62,10 +68,21 @@ import com.vaadin.flow.shared.util.SharedUtil;
 public abstract class VaadinPortlet<C extends Component> extends GenericPortlet
          implements ExportsWebComponent<C> {
 
+    private static final String ACTION_STATE = "state";
+    private static final String ACTION_MODE = "mode";
+
     private VaadinPortletService vaadinService;
     private String webComponentProviderURL;
     private String webComponentBootstrapHandlerURL;
     private String webComponentUIDLRequestHandlerURL;
+
+
+    private WindowState windowState = WindowState.UNDEFINED;
+    private PortletMode portletMode = PortletMode.UNDEFINED;
+
+    private ActionURL actionURL;
+
+    private boolean isPortlet3 = false;
 
     @Override
     public void init(PortletConfig config) throws PortletException {
@@ -126,6 +143,22 @@ public abstract class VaadinPortlet<C extends Component> extends GenericPortlet
     protected void portletInitialized() throws PortletException {
 
     }
+    @Override
+    public void renderHeaders(HeaderRequest request, HeaderResponse response)
+            throws PortletException, IOException {
+        // This is only called for portlet 3.0 portlets.
+        isPortlet3 = true;
+        response.addDependency("PortletHub", "javax.portlet", "3.0.0");
+    }
+
+    @Override
+    public void render(RenderRequest request, RenderResponse response)
+            throws PortletException, IOException {
+        super.render(request, response);
+        if(!isPortlet3 && actionURL == null) {
+            actionURL = response.createActionURL();
+        }
+    }
 
     @Override
     protected void doDispatch(RenderRequest request, RenderResponse response)
@@ -161,22 +194,6 @@ public abstract class VaadinPortlet<C extends Component> extends GenericPortlet
         String portalInfo = portalContext.getPortalInfo()
                 .toLowerCase(Locale.ROOT).trim();
         VaadinPortletService service = getService();
-        //
-        //        if (portalInfo.contains("gatein")) {
-        //            return new VaadinGateInRequest(request, service);
-        //        }
-        //
-        //        if (portalInfo.contains("liferay")) {
-        //            return new VaadinLiferayRequest(request, service);
-        //        }
-        //
-        //        if (portalInfo.contains("websphere portal")) {
-        //            return new VaadinWebSpherePortalRequest(request, service);
-        //        }
-        //        if (portalInfo.contains("weblogic portal")) {
-        //            return new VaadinWebLogicPortalRequest(request, service);
-        //        }
-
         return new VaadinPortletRequest(request, service);
     }
 
@@ -187,24 +204,33 @@ public abstract class VaadinPortlet<C extends Component> extends GenericPortlet
 
     @Override
     public void serveResource(ResourceRequest request,
-            ResourceResponse response) throws PortletException, IOException {
+            ResourceResponse response) throws PortletException {
         handleRequest(request, response);
     }
 
     @Override
     public void processAction(ActionRequest request, ActionResponse response)
-            throws PortletException, IOException {
-        handleRequest(request, response);
+            throws PortletException {
+        if (!isPortlet3 && request.getActionParameters().getNames()
+                .contains(ACTION_STATE)) {
+            WindowState windowState = new WindowState(
+                    request.getActionParameters().getValue(ACTION_STATE));
+            PortletMode portletMode = new PortletMode(
+                    request.getActionParameters().getValue(ACTION_MODE));
+
+            response.setWindowState(windowState);
+            response.setPortletMode(portletMode);
+        }
     }
 
     @Override
     public void processEvent(EventRequest request, EventResponse response)
-            throws PortletException, IOException {
+            throws PortletException {
         handleRequest(request, response);
     }
 
     protected void handleRequest(PortletRequest request,
-            PortletResponse response) throws PortletException, IOException {
+            PortletResponse response) throws PortletException {
 
         CurrentInstance.clearAll();
 
@@ -291,6 +317,62 @@ public abstract class VaadinPortlet<C extends Component> extends GenericPortlet
 
     public String getWebComponentUIDLRequestHandlerURL() {
         return webComponentUIDLRequestHandlerURL;
+    }
+
+    /**
+     * Set a new window state for this portlet
+     *
+     * @param windowState
+     *         window state to set
+     */
+    public void setWindowState(WindowState windowState) {
+        if (isPortlet3) {
+            StringBuilder stateChange = new StringBuilder();
+            stateChange.append(PortletHubUtil.getHubString());
+            stateChange.append("var state = hub.newState();");
+            stateChange.append(String
+                    .format("state.windowState = '%s';", windowState));
+            stateChange.append(String
+                    .format("state.portletMode = '%s';", portletMode));
+            stateChange.append("hub.setRenderState(state);");
+            stateChange.append(PortletHubUtil.getReloadPoller());
+
+            UI.getCurrent().getElement().executeJs(stateChange.toString());
+        } else if (actionURL != null) {
+            String stateChangeScript = String
+                    .format("location.href = '%s?%s=%s&%s=%s'",
+                            actionURL.toString(), ACTION_STATE, windowState, ACTION_MODE,  portletMode);
+
+            UI.getCurrent().getPage().executeJs(stateChangeScript);
+        }
+    }
+
+    /**
+     * Set a new portlet mode for this portlet.
+     *
+     * @param portletMode
+     *         portlet mode to set
+     */
+    public void setPortletMode(PortletMode portletMode) {
+        if (isPortlet3) {
+            StringBuilder modeChange = new StringBuilder();
+            modeChange.append(PortletHubUtil.getHubString());
+            modeChange.append("var state = hub.newState();");
+            modeChange.append(String
+                    .format("state.portletMode = '%s';", portletMode));
+            modeChange.append(String
+                    .format("state.windowState = '%s';", windowState));
+            modeChange.append("hub.setRenderState(state);");
+            modeChange.append(PortletHubUtil.getReloadPoller());
+
+            UI.getCurrent().getElement().executeJs(modeChange.toString());
+        } else if (actionURL != null) {
+            String stateChangeScript = String
+                    .format("location.href = '%s?state=%s&mode=%s'",
+                            actionURL.toString(), windowState, portletMode);
+
+            UI.getCurrent().getPage().executeJs(stateChangeScript);
+        }
     }
 
 }
---
+++ vaadin-portlet/src/main/java/com/vaadin/flow/portal/util/PortletHubUtil.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.portal.util;
+
+import java.util.Collections;
+import java.util.Map;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.portal.VaadinPortletService;
+
+public final class PortletHubUtil {
+
+    private PortletHubUtil() {
+    }
+
+    /**
+     * Register this portlet to the PortletHub.
+     * <p>
+     * hub registration will be stored
+     */
+    public static void registerHub() {
+        registerHub(Collections.emptyMap());
+    }
+
+    /**
+     * Register this portlet to the PortletHub.
+     *
+     * @param eventListeners
+     *         event listeners to register for this portlet. eventListeners
+     *         should be in the format EventName, Event function payload
+     *         which will get params type and state
+     */
+    public static void registerHub(Map<String, String> eventListeners) {
+        String portletRegistryName = VaadinPortletService.getCurrentResponse()
+                .getPortletResponse().getNamespace();
+        StringBuilder register = new StringBuilder();
+
+        register.append("if (!window.Vaadin.Flow.Portlets) {");
+        register.append(
+                "window.Vaadin.Flow['Portlets'] = {};");
+        register.append("}");
+        register.append("if (!window.Vaadin.Flow.Portlets.$0) {");
+        register.append("if (portlet) {");
+        register.append("portlet.register($0).then(function (hub) {");
+        register.append("window.Vaadin.Flow.Portlets[$0] = hub;");
+        register.append(
+                "hub.addEventListener('portlet.onStateChange', function (type, state) {});");
+        eventListeners.forEach((event, functionPayload) -> {
+            register.append("hub.addEventListener('").append(event)
+                    .append("',");
+            register.append("function(type, state){").append(functionPayload)
+                    .append("});");
+        });
+        register.append("});");
+        register.append("}");
+        register.append("}");
+
+        UI.getCurrent().getElement()
+                .executeJs(register.toString(), portletRegistryName);
+    }
+
+    /**
+     * Get JavaScript string for getting the portlet hub registration object.
+     * @return registration object stored as 'hub'
+     */
+    public static String getHubString() {
+        String portletRegistryName = VaadinPortletService.getCurrentResponse()
+                .getPortletResponse().getNamespace();
+        return String.format("var hub = window.Vaadin.Flow.Portlets['%s'];",
+                portletRegistryName);
+    }
+
+
+    /**
+     * This is a script that will handle page reload for page when hub has
+     * completed.
+     *
+     * @return portlet reload poller
+     */
+    public static String getReloadPoller() {
+        StringBuilder reloader = new StringBuilder();
+        reloader.append("const poller = () => {");
+        reloader.append("  if(hub.isInProgress()) {");
+        reloader.append("    setTimeout(poller, 10);");
+        reloader.append("  } else {");
+        reloader.append("    location.reload();");
+        reloader.append("  }");
+        reloader.append("};");
+        reloader.append("poller();");
+        return reloader.toString();
+    }
+
+}

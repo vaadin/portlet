d96d1395727a60bf28c08f5deb8113671bc6f6e5
Tomi Virtanen <tltv@vaadin.com>
2021-05-11 14:42:45 +0300

    Reading ApplicationRouteRegistry without VaadinServletContext
    ApplicationRouteRegistry.createRegistry(VaadinContext) depends on
    VaadinServletContext which is not ok with portlet context.

---
+++ vaadin-portlet/src/main/java/com/vaadin/flow/server/startup/PortletApplicationRouteRegistryUtil.java
@@ -0,0 +1,43 @@
+package com.vaadin.flow.server.startup;
+
+import com.vaadin.flow.portal.VaadinPortletContext;
+import com.vaadin.flow.server.VaadinContext;
+import com.vaadin.flow.server.startup.ApplicationRouteRegistry.ApplicationRouteRegistryWrapper;
+
+public class PortletApplicationRouteRegistryUtil {
+
+    /**
+     * Gets the route registry for the given Vaadin context. If the Vaadin
+     * context has no route registry, a new instance is created and assigned to
+     * the context.
+     *
+     * @param context
+     *            the vaadin context for which to get a route registry, not
+     *            <code>null</code>
+     * @return a registry instance for the given servlet context, not
+     *         <code>null</code>
+     */
+    public static ApplicationRouteRegistry getInstance(
+            VaadinPortletContext context) {
+        assert context != null;
+
+        ApplicationRouteRegistryWrapper attribute;
+        synchronized (context) {
+            attribute = context
+                    .getAttribute(ApplicationRouteRegistryWrapper.class);
+
+            if (attribute == null) {
+                attribute = new ApplicationRouteRegistryWrapper(
+                        createRegistry(context));
+                context.setAttribute(attribute);
+            }
+        }
+
+        return attribute.getRegistry();
+    }
+
+    private static ApplicationRouteRegistry createRegistry(
+            VaadinContext context) {
+        return new ApplicationRouteRegistry();
+    }
+}

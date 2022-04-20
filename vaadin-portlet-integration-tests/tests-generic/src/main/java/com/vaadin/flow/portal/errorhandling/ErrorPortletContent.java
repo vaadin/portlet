801419717859a9dc88406bb5f8c91b4538d3749b
Mikael Grankvist <mikael.grankvist@vaadin.com>
2022-04-20 14:57:29 +0300

    chore: update license headers to CVDLv4

--- vaadin-portlet-integration-tests/tests-generic/src/main/java/com/vaadin/flow/portal/errorhandling/ErrorPortletContent.java
+++ vaadin-portlet-integration-tests/tests-generic/src/main/java/com/vaadin/flow/portal/errorhandling/ErrorPortletContent.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (C) 2019-2022 Vaadin Ltd
+ *
+ * This program is available under Commercial Vaadin Developer License
+ * 4.0 (CVDLv4).
+ *
+ *
+ * For the full License, see <https://vaadin.com/license/cvdl-4.0>.
+ */
 package com.vaadin.flow.portal.errorhandling;
 
 import com.vaadin.flow.component.button.Button;
